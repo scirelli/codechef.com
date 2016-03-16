314a7b2845ee1740726497e5e001320400c8a0a2
Steve Cirelli <scirelli+git@gmail.com>
2016-03-15 23:23:29 -0400

    Update main.js
    Attempt 1

--- nonDecreasingSubarrays/main.js
+++ nonDecreasingSubarrays/main.js
@@ -2,7 +2,8 @@
 /**
 * Given an array A1, A2, ..., AN, count the number of subarrays of array A which are non-decreasing.
 * A subarray A[i,j], where 1 <= i <= N is a sequence of integers Aj, Aj+1, ..., Aj
-* A subarray A[i,j] is non-decreasing if Ai <= Ai+1 <= ... <= Aj. You have to count the total number of such arrays.
+* A subarray A[i,j] is non-decreasing if Ai <= Ai+1 <= ... <= Aj. 
+* You have to count the total number of such arrays.
 * 
 * Input:
 *   The first of input contains an integer T denoting the number of test cases. The description of T test cases follows.
@@ -38,32 +39,38 @@
 *    Example case 1.
 *       All valid subarrays are A[1,1], A[1,2], A[2,2], A[3, 3], A[3, 4], A[4, 4] 
                                   [1] ,  [1,4],  [4]  ,   [2]  ,   [2,3],  [3]
+*    
 *    Note that singleton subarrays are identically non-decreasing
-*
-*  Only single subarray A[1, 1] is non-decreasing.
+* 
+*    Example case 2
+*       Only single subarray A[1, 1] is non-decreasing.
+*                               [5]
 */
 function require(){ return { createInterface:function(){ return {on:function(str, fnc){
-    fnc('2');
+    fnc('5');
 
     fnc('4');
     fnc('1 4 2 3');
 
     fnc('1');
     fnc('5');
-/*
+
     fnc('4');
-    fnc('1 4 2 3');
+    fnc('4 3 2 1');
  
-    fnc('1');
-    fnc('5');
+    fnc('2');
+    fnc('1 4');
 
-    fnc('1');
-    fnc('5');
-*/
-}}}}}; 
+    fnc('3');
+    fnc('1 2 3');
+}}}}};
+var process = {stdin:'', stdout:'' };
 
 var readline = require("readline");
-var reader = readline.createInterface({}),
+var reader = readline.createInterface({
+        input : process.stdin,
+        output : process.stdout
+    }),
     nInputLine = 0,
     nTestCases = 0,
     nArraySize  = 0;
@@ -81,7 +88,7 @@ reader.on( "line", function(data) {
         }else if( nInputLine%2 === 1){
             nTestCases--;
             data = data.replace(/s+/,' ').split(' ').map(Number);
-            console.log(data);
+            //console.log(data);
             console.log(countSubArrays(data));
         }
     }
@@ -93,22 +100,55 @@ reader.on( "line", function(data) {
 });
 
 //'1 4 2 3'
+//'4 3 2 1'
 //[1] ,  [1,4],  [4]  ,   [2]  ,   [2,3],  [3]
 function countSubArrays( aArray ){
-    var nSubArrays = 1,
-        nSubs=1;
+    var nSubs=0;
 
-    for( var i=0, l=aArray.length,num,tmp,next; i<l; i++){
-        num = aArray[i];
+    for( var i=0, cur=0, next=0, end=aArray.length,start=0; i<end; i++){
+        cur = aArray[i];
         next = aArray[i+1];
-
-        if( num <= next ){
-            nSubs++;
-        }else{
-            nSubArrays += nSubs;
-            nSubs=1;
+        if( next === undefined || cur > next ){
+            nSubs += ((i-start)+1) > 1 ? 3 : 1;
+            start = i+1;
         }
     }
     
-    return nSubArrays;
+    return nSubs;
 };
+
+/* C++ accepted answer
+#include <iostream>
+ 
+unsigned long long ctr[5];
+using namespace std;
+void countsub(unsigned long long*,unsigned int,unsigned long long*,int t);
+int main()
+{
+    ios_base::sync_with_stdio(false);
+  unsigned int n;int t;
+  cin>>t;
+  for(int i=0;i<t;++i)
+  {
+  cin>>n;
+  unsigned long long* arr=new unsigned long long[n];
+  for(unsigned int i=0;i<n;++i)
+    cin>>arr[i];
+  countsub(arr,n,ctr,i);
+  }
+  for(int i=0;i<t;++i)
+    cout<<ctr[i]<<endl;
+    return 0;
+}
+void countsub(unsigned long long* arr,unsigned int n,unsigned long long*,int t)
+{
+    unsigned int j=1;unsigned long long k=1;
+    ctr[t]=1;
+    while(j<n)
+    {
+        if(arr[j]>=arr[j-1])
+            {ctr[t]+=++k;++j;}
+        else {j++;k=1;++ctr[t];}
+    }
+}
+*/
